17d353557ed07ac79ad4e956ee758d23d7e6ad98
Jason Wang <jason.ruonan.wang@gmail.com>
2025-11-20 16:42:56 +0800

    Initial Commit

---
+++ FckHdmiDevice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FckHdmiDevice
+//
+//  Created by Jason Wang on 2025/11/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
